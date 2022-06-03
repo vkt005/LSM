d6ea195975e0617ca8e783c6fcab8314e307b05a
shivani panwar <shivani.p@chapter247.com>
2022-06-03 06:59:40 +0530

    added webinar apis

--- server/routes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-let { registerAPIRoutes } = require('./resources/routes');
-module.exports = function (app) {
-  // Register public and API sepcific routes here
-  registerAPIRoutes(app);
-};
-
